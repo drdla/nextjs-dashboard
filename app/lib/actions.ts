a076b8828ba02b7b244e7e3d45712cd98c3e1e05
Dominik Rodler <dominik.rodler@gmail.com>
2024-03-10 14:49:21 +0100

    Code up to and including chapter 14 (form validation)
    TODO: https://nextjs.org/learn/dashboard-app/improving-accessibility#practice-adding-aria-labels

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,9 +7,15 @@ import { z } from 'zod';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
@@ -23,28 +29,53 @@ const UpdateInvoice = FormSchema.omit({
   date: true,
 });
 
-export async function createInvoice(formData: FormData) {
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
   // const rawFormData = {
   //   customerId: formData.get('customerId'),
   //   amount: formData.get('amount'),
   //   status: formData.get('status'),
   // };
   // console.log(rawFormData);
-
-  const { amount, customerId, status } = CreateInvoice.parse({
+  const validatedFields = CreateInvoice.safeParse({
     amount: formData.get('amount'),
     customerId: formData.get('customerId'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to create invoice.',
+    };
+  }
+
+  const { amount, customerId, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO
-      invoices (customer_id, amount, status, date)
-    VALUES
-      (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO
+        invoices (customer_id, amount, status, date)
+      VALUES
+        (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to create invoice.',
+    };
+  }
 
   const invoicesRoute = '/dashboard/invoices';
   revalidatePath(invoicesRoute);
@@ -58,17 +89,22 @@ export async function updateInvoice(id: string, formData: FormData) {
     status: formData.get('status'),
   });
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    UPDATE invoices
-    SET
-      amount = ${amountInCents},
-      customer_id = ${customerId},
-      status = ${status}
-    WHERE
-      id = ${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET
+        amount = ${amountInCents},
+        customer_id = ${customerId},
+        status = ${status}
+      WHERE
+        id = ${id}
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
+  }
 
   const invoicesRoute = '/dashboard/invoices';
   revalidatePath(invoicesRoute);
@@ -76,12 +112,24 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`
-    DELETE FROM
-      invoices
-    WHERE
-      id = ${id}
-  `;
+  throw new Error('Failed to Delete Invoice');
+
+  try {
+    await sql`
+      DELETE FROM
+        invoices
+      WHERE
+        id = ${id}
+    `;
+
+    return {
+      message: `Deleted Invoice ${id}.`,
+    };
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.',
+    };
+  }
 
   const invoicesRoute = '/dashboard/invoices';
   revalidatePath(invoicesRoute);
